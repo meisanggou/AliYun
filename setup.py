8ef571c7ae0f71b46eb09bbef12dc796211cce81
meisanggou <zhou5315938@163.com>
2017-04-06 16:51:28 +0800

    add install requires

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.version_info <= (2, 7):
     sys.exit(1)
 
 name = "JYAliYun"
-version = "0.1.4"
+version = "0.1.5"
 url = "https://github.com/meisanggou/AliYun"
 license = "MIT"
 author = "meisanggou"
