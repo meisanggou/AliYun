186e11e5de57418c2328ca146c02f8b93797ad8b
meisanggou <zhou5315938@163.com>
2017-05-02 10:49:10 +0800

    新OSS

--- JYAliYun/AliYunOSS/__init__.py
+++ JYAliYun/AliYunOSS/__init__.py
@@ -2,47 +2,60 @@
 # coding: utf-8
 
 from time import time
-import base64
-import hmac
-import hashlib
 from urllib import quote
+from JYAliYun import AliYUN_DOMAIN_NAME
 from JYAliYun.AliYunObject import ObjectManager
-from JYAliYun.Tools import ali_signature
+from JYAliYun.Tools import ali_signature, jy_requests
 
 __author__ = 'meisanggou'
 
 
-class OSSObject(ObjectManager):
-    def sign_url(self, endpoint, bucket_name, key, expires=60):
-        url = "http://%s.%s/%s" % (bucket_name, quote(endpoint, ''), quote(key, ''))
-        Expires = "%s" % int(time() + expires)
-        resource_string = "/%s/%s" % (bucket_name, key)
-        signature = ali_signature(self.access_key_secret, "GET", "", "", Expires, "", resource_string)
-        url += "?OSSAccessKeyId=%s&Expires=%s&Signature=%s" % (self.access_key_id, Expires, quote(signature, ""))
-        return url
-
-    def sing_file_url(self, bucket_name, key, expires=60):
-        url = "http://file.gene.ac/%s" % quote(key, '')
-        Expires = "%s" % int(time() + expires)
-        resource_string = "/%s/%s" % (bucket_name, key)
-        signature = ali_signature(self.access_key_secret, "GET", "", "", Expires, "", resource_string)
-        url += "?OSSAccessKeyId=%s&Expires=%s&Signature=%s" % (self.access_key_id, Expires, quote(signature, ""))
-        return url
-
-
 class OSSBucket(ObjectManager):
-    def __init__(self, bucket_name, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
+        kwargs["default_section"] = "OSS"
         super(OSSBucket, self).__init__(*args, **kwargs)
-        self.bucket_name = bucket_name
+        if self.cfg.get("bucket") is not None:
+            self.bucket_name = self.cfg.get('bucket')
+        else:
+            self.bucket_name = kwargs["bucket_name"]
+        self.region = self.cfg.get("region", "beijing")
+        self.protocol = self.cfg.get("protocol", "http")
+        if self.is_internal is True:
+            self.endpoint = "oss-cn-%s-internal.%s" % (self.region, AliYUN_DOMAIN_NAME)
+        else:
+            self.endpoint = "oss-cn-%s.%s" % (self.region, AliYUN_DOMAIN_NAME)
+        self.server_url = "%s.%s" % (self.bucket_name, self.endpoint)
 
     @staticmethod
     def get_resource(bucket_name, key):
         return "/%s/%s" % (bucket_name, key)
 
-    def sing_file_url(self, key, expires=60):
-        url = "http://file.gene.ac/%s" % quote(key, '')
-        Expires = "%s" % int(time() + expires)
-        resource_string = OSSBucket.get_resource(self.bucket_name, key)
-        signature = ali_signature(self.access_key_secret, "GET", "", "", Expires, "", resource_string)
-        url += "?OSSAccessKeyId=%s&Expires=%s&Signature=%s" % (self.access_key_id, Expires, quote(signature, ""))
-        return url
+    @staticmethod
+    def format_key(key):
+        return key.lstrip("/")
+
+    def sing_file_url(self, key, method="GET", expires=60, server_url=None):
+        key = OSSBucket.format_key(key)
+        sign_url = self.protocol + "://"
+        if server_url is not None:
+            sign_url += server_url
+        else:
+            sign_url += self.server_url
+        sign_url += "/%s" % quote(key, '')
+        expires = "%s" % int(time() + expires)
+        resource_string = "/%s/%s" % (self.bucket_name, key)
+        signature = ali_signature(self.access_key_secret, method, "", "", expires, "", resource_string)
+        sign_url += "?OSSAccessKeyId=%s&Expires=%s&Signature=%s" % (self.access_key_id, expires, quote(signature, ""))
+        return sign_url
+
+    def head_object(self, oss_object):
+        if not oss_object.startswith("/"):
+            oss_object = "/" + oss_object
+        oss_resource = "/%s%s" % (self.bucket_name, oss_object)
+
+        url = "http://%s%s" % (self.oss_host, oss_object)
+        headers = ali_signature(self._access_id, self._access_secret, "HEAD", oss_resource)
+        response = jy_requests.head(url)
+        if response.status_code == 200:
+            return True, response.headers
+        return False, response.status_code
--- demo/aliyun_oss.py
+++ demo/aliyun_oss.py
@@ -2,23 +2,27 @@
 # coding: utf-8
 
 from JYAliYun.AliYunAccount import RAMAccount
-from JYAliYun.AliYunOSS import OSSObject
+from JYAliYun.AliYunOSS import OSSBucket
 
 __author__ = 'ZhouHeng'
 
 """
-配置文件示例 /data/Web2/conf/oss.conf
+配置文件示例 conf/oss.conf
 [Account]
 access_key_id: LTA***u**AqEZ0H6
 access_key_secret: 25*TaQ*EQPx*Ck*Jo*gKuspzPk7pzw
 internal: false  #  是否为阿里内网
 
+[OSS]
+bucket: geneac
+region: beijing #  不存在时默认为beijing
+
 """
 
 """
-发送 主题消息
+生成oss文件授权链接
 """
-conf_dir = "/data/Web2/conf"
+conf_dir = "conf"
 oss_account = RAMAccount(conf_dir=conf_dir, conf_name="oss.conf")
-oss_man = OSSObject(ram_account=oss_account)
-print oss_man.sing_file_url("geneac", "dmsdata/editor/img/20173/zh_test_166_1490249799.png")
+bucket_man = OSSBucket(ram_account=oss_account, conf_dir=conf_dir, conf_name="oss.conf")
+print bucket_man.sing_file_url("dmsdata/editor/img/20173/zh_test_166_1490249799.png", server_url="file.gene.ac")
