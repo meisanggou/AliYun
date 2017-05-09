7e006a82faee48b1c6f00a1278b1d7f3f8245c70
meisanggou <zhou5315938@163.com>
2017-05-09 15:39:48 +0800

    fix bug 0.1.8

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.version_info <= (2, 7):
     sys.exit(1)
 
 name = "JYAliYun"
-version = "0.1.7"
+version = "0.1.8"
 url = "https://github.com/meisanggou/AliYun"
 license = "MIT"
 author = "meisanggou"
