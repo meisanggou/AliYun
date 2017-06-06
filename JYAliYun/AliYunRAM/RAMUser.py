400647b569bda46faf75b80bde7b7d3880fda696
meisanggou <zhou5315938@163.com>
2017-06-06 15:26:51 +0800

    use create delete get

--- JYAliYun/AliYunRAM/RAMUser.py
+++ JYAliYun/AliYunRAM/RAMUser.py
@@ -16,7 +16,27 @@ class RAMUserManager(ObjectManager):
         action = "GetUser"
         http_method = "GET"
         custom_params = dict(Action=action, UserName=user_name)
-        print(self.access_key_id)
+        params = get_params(self.access_key_id, self.access_key_secret, http_method, custom_params)
+        resp = jy_requests.request(http_method, self.address, params=params)
+        return resp
+
+    def create_user(self, user_name, **kwargs):
+        action = "CreateUser"
+        http_method = "GET"
+        custom_params = dict(Action=action, UserName=user_name)
+        allow_keys = {"display_name": "DisplayName", "mobile_phone": "MobilePhone", "email": "Email",
+                      "comments": "Comments"}
+        for key in allow_keys.keys():
+            if key in kwargs:
+                custom_params[allow_keys[key]] = kwargs[key]
+        params = get_params(self.access_key_id, self.access_key_secret, http_method, custom_params)
+        resp = jy_requests.request(http_method, self.address, params=params)
+        return resp
+
+    def delete_user(self, user_name):
+        action = "DeleteUser"
+        http_method = "GET"
+        custom_params = dict(Action=action, UserName=user_name)
         params = get_params(self.access_key_id, self.access_key_secret, http_method, custom_params)
         resp = jy_requests.request(http_method, self.address, params=params)
         return resp
--- demo/aliyun_ram.py
+++ demo/aliyun_ram.py
@@ -8,5 +8,12 @@ __author__ = 'meisanggou'
 
 ram_account = RAMAccount(conf_path="/data/Web2/conf/oss.conf")
 user_man = RAMUserManager(ram_account=ram_account)
-r_resp = user_man.get_user("api")
+
+resp = user_man.create_user("api_test")
+print(resp.text)
+
+r_resp = user_man.get_user("api_test")
+print(r_resp.text)
+
+r_resp = user_man.delete_user("api_test")
 print(r_resp.text)
