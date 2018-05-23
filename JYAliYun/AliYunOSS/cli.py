836f9a69980345c3a7b318aad9cf4d0b75f54638
meisanggou <zhou5315938@163.com>
2018-05-23 09:38:05 +0800

    0.3.1

--- JYAliYun/AliYunOSS/cli.py
+++ JYAliYun/AliYunOSS/cli.py
@@ -11,11 +11,39 @@ __author__ = '鹛桑够'
 env_oss_conf = "OSS_CONF_PATH"
 
 
+def print_table(t):
+    def char_len(c):
+        return len(c)
+
+    def calc_tab(l, down=False):
+        n = l / 8
+        if l % 8 != 0 and down is False:
+            n += 1
+        return n
+    max_len = []
+    for line in t:
+        for i in range(len(line)):
+            l_c = char_len(line[i])
+            if i >= len(max_len):
+                max_len.append(l_c)
+            elif l_c > max_len[i]:
+                max_len[i] = l_c
+    tabs = []
+    for m_item in max_len:
+        tabs.append(calc_tab(m_item) + 1)
+    for line in t:
+        line_s = []
+        for i in range(len(line)):
+            p_s = line[i] + "\t" * (tabs[i] - calc_tab(char_len(line[i]), down=True))
+            line_s.append(p_s)
+        print("".join(line_s))
+
+
 def receive_conf_path(conf_path):
     if conf_path is None:
         conf_path = os.environ.get(env_oss_conf)
         if conf_path is None:
-            sys.stderr.write("Please use -c or --conf-path set oss configure path")
+            sys.stderr.write("Please use -c or --conf-path set oss configure path\n")
             sys.exit(1)
     return conf_path
 
@@ -45,8 +73,10 @@ def oss_head():
             print("head fail! oss server return %s" % resp.status_code)
             exit_code += 1
             continue
+        t = [["Header Key", "Header Value"], ["------------", "------------"]]
         for k, v in resp.headers.items():
-            print("%s\t%s" % (k, v))
+            t.append([k, v])
+        print_table(t)
     sys.exit(exit_code)
 
 
